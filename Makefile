b29fd9b7c27d88594d599672b2b04d3b3dda67b8
Dan Farrell <dfarrell@doctorondemand.com>
2019-02-21 00:32:53 -0600

    progress meter every 10 seconds

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
+test:
+	tmpfile=$$(mktemp); \
+	trap "rm -v $$tmpfile"  EXIT; \
+	dd bs=1m count=10 of=$$tmpfile if=/dev/urandom;\
+	md5sum < $$tmpfile;\
+	go run gogcs.go develop-203820-junk test <$$tmpfile; \
+	gsutil cp gs://develop-203820-junk/test - |md5sum
+
 linux:
 	GOOS=linux GOARCH=amd64 go build gogcs.go
+
--- gogcs.go
+++ gogcs.go
@@ -1,7 +1,3 @@
-/* 
-  goal: read standard input and write to gcs
-*/
-
 package main
 
 import(
@@ -10,7 +6,34 @@ import(
   "os"
   "io"
   "cloud.google.com/go/storage"
+  "time"
+  //"flag"
 )
+
+type ProgressWriter struct {
+    io.Writer
+    total uint64
+    increment time.Duration
+    start time.Time
+    last time.Time
+}
+
+func (pt *ProgressWriter) Write(p []byte) (int, error) {
+  if pt.total == 0 {
+    pt.start = time.Now()
+  }
+  n, err := pt.Writer.Write(p)
+  pt.total += uint64(n)
+  if err == nil {
+    now := time.Now()
+    if now.Sub(pt.last) > ( 10 * time.Second ) {
+      log.Println("Wrote", pt.total, "bytes in", now.Sub(pt.start) )
+      pt.last = now
+    }
+  }
+  return n, err
+}
+
 
 func main() {
   var gcsc *storage.Client
@@ -24,7 +47,7 @@ func main() {
   obj := gcsc.Bucket(bucket).Object(dest)
   w := obj.NewWriter(ctx)
   defer w.Close()
-  bytes, err := io.Copy(w, os.Stdin)
+  bytes, err := io.Copy(&ProgressWriter{Writer:w, increment: 1 * time.Second }, os.Stdin)
   if err != nil {
     log.Fatal(err)
   } else {
